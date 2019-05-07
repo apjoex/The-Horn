e199741796226588197d3dd38a091a606d84411b
Joseph Akinde-peters <apjoex@gmail.com>
2019-05-07 09:53:08 +0100

    +NumbersActivity added

---
+++ app/src/main/java/com/yahya/thehorn/ui/NumbersActivity.kt
@@ -0,0 +1,13 @@
+package com.yahya.thehorn.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.yahya.thehorn.R
+
+class NumbersActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_numbers)
+    }
+}
